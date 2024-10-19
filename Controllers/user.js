03a0e44d3fb57f6ce119f392354a9075c4a79202
chetan14203 <109654816+chetan14203@users.noreply.github.com>
2024-10-19 18:21:11 +0530

    Updated home image size and made other style improvements

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,30 +1,38 @@
-const { getConnection } = require('../connectDB'); // Import the getConnection function
+const { getConnection } = require('../connectDB'); 
 
-// Controller to handle pickup creation
 exports.createPickup = async (req, res) => {
   const { mobileNumber, name, address, pickupDate, pickupTime } = req.body;
-
-  // SQL query to insert pickup details
   const sql = 'INSERT INTO pickups (mobileNumber, name, address, pickupDate, pickupTime) VALUES (?, ?, ?, ?, ?)';
-
   let connection;
   try {
-    // Get a connection from the pool
     connection = await getConnection();
-    
-    // Execute the query using the connection
     const [results] = await connection.query(sql, [mobileNumber, name, address, pickupDate, pickupTime]);
-
-    // Respond with success message and the inserted ID
     res.status(201).json({ message: 'Pickup details received', id: results.insertId });
   } catch (error) {
     console.error('Error inserting data:', error);
     res.status(500).json({ error: 'Database error' });
   } finally {
-    // Release the connection back to the pool
     if (connection) {
       connection.release();
     }
   }
 };
 
+exports.franchise = async (req, res) => {
+  const { name, address, mobileNumber, vehicleName, vehicleNumber } = req.body;
+
+  try {
+    const franchise = new Franchise({
+      name,
+      address,
+      mobileNumber,
+      vehicleName,
+      vehicleNumber,
+    });
+
+    await franchise.save();
+    res.status(201).send({ message: 'Franchise registered successfully!' });
+  } catch (error) {
+    res.status(500).send({ error: 'Failed to register franchise' });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const pickupController = require('../Controllers/user');
 
 
 router.post('/pickup', pickupController.createPickup);
+router.post('/franchise',pickupController.franchise);
 
 module.exports = router;
